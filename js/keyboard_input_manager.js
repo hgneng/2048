c1dbcdc7c29755947f06d664faf6017d24af81f3
Cameron Wong <hgneng@yahoo.com.cn>
2016-12-09 16:26:45 +0800

    Update keyboard_input_manager.js

--- js/keyboard_input_manager.js
+++ js/keyboard_input_manager.js
@@ -1,6 +1,8 @@
+// 从文件名，我们可以猜测，这个文件的作用是处理键盘输入的
 function KeyboardInputManager() {
   this.events = {};
 
+  // 不同的浏览器有不同的事件
   if (window.navigator.msPointerEnabled) {
     //Internet Explorer 10 style
     this.eventTouchstart    = "MSPointerDown";
@@ -15,13 +17,18 @@ function KeyboardInputManager() {
   this.listen();
 }
 
+// 我们定义KeyboardInputManager.on(event, callback)这个方法
+// 它可以监听event事件，例如键盘按下、鼠标点击等，当事件触发的时候就执行
 KeyboardInputManager.prototype.on = function (event, callback) {
   if (!this.events[event]) {
     this.events[event] = [];
   }
-  this.events[event].push(callback);
+  this.events[event].push(callback); // 注册的回调函数是以数组方式保存的，后注册的并不会覆盖之前注册的，只是按顺序执行回调函数
+                                     // 例如，我们可以在一个地方注册某个格子点击后回闪一下，在另外一个地方注册同一个格子点击后发成“滴”一声
+                                     // 那么实际点击该格子，就会既闪一下，又发出“滴”一声
 };
 
+// emit方法触发事件的回调函数，并以data作为参数逐一执行回调函数。
 KeyboardInputManager.prototype.emit = function (event, data) {
   var callbacks = this.events[event];
   if (callbacks) {
@@ -34,6 +41,8 @@ KeyboardInputManager.prototype.emit = function (event, data) {
 KeyboardInputManager.prototype.listen = function () {
   var self = this;
 
+  // 下面是键盘键对应的ASCII码，从这个列表我们可以发现，原来不单止上下左右方向键可以操作，WSAD也可以。
+  // 事实上，在PC双打的游戏中WSAD就是常用的第二个玩家的方向键
   var map = {
     38: 0, // Up
     39: 1, // Right
@@ -77,6 +86,7 @@ KeyboardInputManager.prototype.listen = function () {
   var touchStartClientX, touchStartClientY;
   var gameContainer = document.getElementsByClassName("game-container")[0];
 
+  // 除了用键盘控制，我们还可以在手机上通过触摸控制，这就是Touchstart事件，和Touchend事件
   gameContainer.addEventListener(this.eventTouchstart, function (event) {
     if ((!window.navigator.msPointerEnabled && event.touches.length > 1) ||
         event.targetTouches.length > 1) {
@@ -120,15 +130,18 @@ KeyboardInputManager.prototype.listen = function () {
     var dy = touchEndClientY - touchStartClientY;
     var absDy = Math.abs(dy);
 
-    if (Math.max(absDx, absDy) > 10) {
+    // 通过计算触摸事件的起止位置差，判断用户是做上下左右哪个方向的移动
+    if (Math.max(absDx, absDy) > 10) { // 移动距离最小是10个单位
       // (right : left) : (down : up)
+      // 调用move方法，0-3作为参数。0表示向上移动，1表示向右移动，2表示向下移动，3表示向左移动
       self.emit("move", absDx > absDy ? (dx > 0 ? 1 : 3) : (dy > 0 ? 2 : 0));
     }
   });
 };
 
 KeyboardInputManager.prototype.restart = function (event) {
-  event.preventDefault();
+  event.preventDefault(); // 阻止事件向上传递。比如说，有个人要闯进公司，守门口的保安把他拦住了，他没有必要把这件事告诉老板。
+                          // 如果他没有把事件拦住，老板可能又会叫另外一个保安过来驱赶这个人。
   this.emit("restart");
 };
 
