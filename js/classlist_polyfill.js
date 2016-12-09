5feaf6e8ca8a0f0401988597cfdff24a2ee2cd6e
Cameron Wong <hgneng@yahoo.com.cn>
2016-12-09 15:56:40 +0800

    Update classlist_polyfill.js

--- js/classlist_polyfill.js
+++ js/classlist_polyfill.js
@@ -1,3 +1,9 @@
+// 整个文件的代码由(function(){})();括起来，这种技巧是为了让文件里的变量不影响其它文件
+// 比如说，如果这个文件定义了变量var i。如果不这样做，另外一个文件也有变量var i，那么就会冲突，并在逻辑上产生意外的结果。
+// 这种技巧在其它现代的面向对象语言（如Java）里是通过名字空间实现的。
+// 补充说明：Java和JavaScript是完全不同的计算机语言，没有任何关系。而C和C++语言则是有很大关系的。
+// 和bind_polifill.js类似，带polifill后缀的3个JS文件都是对不支持某些有用方法的JavaScript解释器的补丁文件。
+// 这个补丁文件是保证JavaScript对象有一个classList的方法可以获取元素的所有class
 (function () {
   if (typeof window.Element === "undefined" ||
       "classList" in document.documentElement) {
