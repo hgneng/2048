219fdbea8e63cad2bbd66513cbc9e52499390e0f
Cameron Wong <hgneng@yahoo.com.cn>
2016-12-15 11:15:37 +0800

    Update local_storage_manager.js

--- js/local_storage_manager.js
+++ js/local_storage_manager.js
@@ -1,3 +1,7 @@
+// localStorage是HTML5的一个特性，用于把网页的一些状态数据保存在浏览器
+// 使用localStorage可以保存游戏的中间状态，即我们玩到一半把浏览器关闭，然后重新打开是不需要重新玩的
+// 有些旧的浏览器不支持localStorage，那么我们就用一个假的localStorage对象来支持。
+// 当然了，这只是不让游戏出错可以正常运行，不支持localStorage的浏览器是不能在重新打开网页时恢复上一次的游戏状态的
 window.fakeStorage = {
   _data: {},
 
